3f9b020ce350f5f9fdc578276b81bd60cffc88c3
DragonQueen <vworri@gmail.com>
2016-12-07 19:34:02 -0500

    First Draft: still needs functionality

--- Eavesdrop.py
+++ Eavesdrop.py
@@ -1,17 +1,21 @@
 # had to install pyshark
 # using Python 2.7.12
-import pyshark
+class pysharkSniffer():
+    import pyshark
+    out_string = ""
 #cap is where I am saving the packets I get from tshark (cap == capture)
 #interface is en0 because I have a mac
 # may make this amendable with simple user interface
-# using a display_filter because I do not know yet how to make it a capture filter from pyshark
-cap = pyshark.LiveCapture(interface='en0', display_filter='http.request.method =="POST"')
+# using a display_filter because I do not know yet how to make it a capture filter in pyshark
+    cap = pyshark.LiveCapture(interface='en0')
 #still need to find better parameter for sniff
-cap.sniff(timeout=50)
-cap
-#saving each captured file into a txt file *should I make it a pcap file?
-for pkt in cap:
-    with open("Saved_Data.txt", "w") as text_file:
-        text_file.write("Packet: {}".format(pkt))
+    cap.sniff(packet_count=50)
+# run command
 
-print ('Done!')
+#saving each captured file into a txt file *should I make it a pcap file?
+    for pkt in cap:
+        out_file = open("Eavesdrop_Data.txt", "w")
+        out_string += str(pkt)
+        out_string += "\n"
+        out_file.write(out_string)
+    cap.close()
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # eavesdrop
-A sniffing app to pull redundant copied of submission forms or other Ethernet traffic. Part of Unhackable Server Project. 
+A sniffing app to pull redundant copied of submission forms or 
+other Ethernet traffic. Part of Unhackable Server Project. 
+
+Eavesdrop, at this point just sits and sniffs. Once you run it,
+ it will keep sniffing and putting data into a text file until you kill it or until it captures 
+ 50 packets.
+ To change the limit, edit:
+        cap.sniff(packet_count=50) 
+ by changing 50 to whatever limit you want.
+  It is programmed to get everything that is on "en0". There is no filter so everything
+ is written in the text file. 
+ The text file is named "Eavesdrop_Data.txt". Every time you run the program, if you do not change the file name,
+ the file will be overwritten with the new data.
+ I will try and write a user interface to give the user some options
