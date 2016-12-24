982b8cd60e90d1711c42afb936095f8f570b43d6
DragonQueen <vworri@gmail.com>
2016-12-24 00:39:45 -0500

    I used "https://codingsec.net/2016/05/pcap-processing-using-python/" to try and reverse engineer something. Still have issues with the headers.

--- Eavesdrop.py
+++ Eavesdrop.py
@@ -3,15 +3,31 @@ from
 
 os.system("tshark  -T fields -e  data.data -e frame.time -w Eavesdrop_Data.pcap > Eavesdrop_Data.txt -F pcap -c 1000")
 
-# run this to give wireshark root access /Applications/Wireshark.app/Contents/MacOS/Wireshark
-data = " Eavesdrop_Data.pcap"
-def http_payload_assembler(data):
-    texts_detected = 0
-    a = s.rdpcap(data)
-    sessions = a.sessions()
-    for session in sessions:
-        http_payload = ""
-        for packet in sessions[session]:
-            if (packet[s.TCP].dport == 80 or packet[s.TCP].sport == 80):
-                http_payload += str(packet[s.TCP].payload)
-                print http_payload
+
+import os
+from scapy.all import *
+
+from getHTTPHeaders import HTTPHeaders, extractText
+data = "Eavesdrop_Data.pcap"
+a = rdpcap(data)
+os.system("tshark  -T fields -e _ws.col.Info -e http -e frame.time -e  "
+          "data.data -w Eavesdrop_Data.pcap > Eavesdrop_Data.txt -c 1000")
+os.system("tshark -r Eavesdrop_Data.pcap -Y http -w Eavesdrop_Data_http.pcap")
+text_directory = "/home/DragonQueen/"
+
+sessions = a.sessions()
+carved_texts = 1
+for session in sessions:
+    http_payload = ""
+    for packet in sessions[session]:
+        try:
+            if packet[TCP].dport == 80 or packet[TCP].sport == 80:
+                http_payload += str(packet[TCP].payload)
+        except:
+            pass
+        headers = HTTPHeaders(http_payload)
+    if headers is None:
+        continue
+    text = extractText(headers,http_payload)
+    if text is not None:
+         print (text)
