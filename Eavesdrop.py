36e3bad07ad661b6064e873e4d69cbd078500c3f
DragonQueen <vworri@gmail.com>
2017-02-08 16:54:44 -0500

    changed the ui added npyscreen.NPSAppManaged and added some modularity for expected expansion

--- Eavesdrop.py
+++ Eavesdrop.py
@@ -2,6 +2,7 @@ import re
 import subprocess
 import json
 from collections import OrderedDict
+
 class Eavesdrop():
     #print device list
     def __init__(self):
@@ -19,8 +20,8 @@ class Eavesdrop():
         p = subprocess.Popen("tshark -h", stdout=subprocess.PIPE, shell=True)
         result = p.communicate()
         self.devList = result[0]
-
-    def parsePacket(pkt):
+#Parse the packets into ordered dict
+    def parsePacket(self,pkt):
         patt = re.compile("[^\n]+")
         x = patt.findall(pkt)
         key = ""
@@ -38,15 +39,22 @@ class Eavesdrop():
                 val2 += l
                 que[key][val1] = val2
         return que
+
     def contSniff(self):
-        p = subprocess.Popen("tshark -V  -l -p  -S '::::END OF PACKET::::::'", stdout=subprocess.PIPE,
+        count = 0
+        data = ""
+        p = subprocess.Popen("tshark -V  -l -p  -S '::::END OF PACKET::::::' ", stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE, shell=True)
-        for line in iter(p.stdout.readline, '\r\n\r\n'):
-            data = ""
+        for line in iter(p.stdout.readline, '\n\r\n'):
             if ('::::END OF PACKET::::::' not in line):
                 data += line
             else:
-                return self.parsePacket(data)
+                packet = data
+                data = ""
+                print self.parsePacket(packet)
+            if "malformed" in data:
+                count += 1
+
 
 
 
--- UserInt.py
+++ UserInt.py
@@ -3,19 +3,38 @@ import npyscreen
 from Eavesdrop import Eavesdrop
 
 contentType = ['multipart/form-data','text','video','audio', 'image']
-class EavesdropForm(npyscreen.Form):
+
+
+class EavesdropApp(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.addForm("MAIN", EavesdropForm, name="Sniffing Parameters")
+
+
+
+
+
+
+
+class EavesdropForm(npyscreen.ActionFormV2):
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
     def create(self):
         self.captureDevice = self.add(npyscreen.TitleSelectOne,max_height=20, name='Capture Device', values= Eavesdrop().getList(),scroll_exit=True)
         self.Contenttype = self.add(npyscreen.TitleSelectOne, max_height=20, name='Content Type',
                                       values=contentType, scroll_exit=True)
 
+    def on_ok(self):
+        #npyscreen.notify_confirm("Starting Sniff:     ")
+        F = EavesdropForm(name="Eavesdrop")
+        F.edit()
+        dev = F.captureDevice.value
+        type = F.Contenttype.value
+        Sniffoutput = Eavesdrop().contSniff()
+        return Sniffoutput
 
-def myFunction(*args):
-    F = EavesdropForm(name = "Eavesdrop")
-    F.edit()
-    dev = F.captureDevice.value
-    type = F.Contenttype.value
-    return dev, contentType[type]
+
+
+
 
 if __name__ == '__main__':
-    print npyscreen.wrapper_basic(myFunction)
+    EavesdropApp().run()
